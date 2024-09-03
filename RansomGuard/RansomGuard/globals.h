5fc9a2294af982d072ebef07323eac07d921a817
Windy Bug <139051196+0mWindyBug@users.noreply.github.com>
2024-09-03 21:59:18 +0300

    Delete RansomGuard directory

--- RansomGuard/RansomGuard/globals.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <fltKernel.h>
-#include "Mutex.h"
-
-extern PFLT_FILTER gFilterHandle;
-extern PFLT_PORT FilterPort;
-extern PFLT_PORT SendClientPort;
-extern Mutex ProcessesListMutex;
-extern EX_RUNDOWN_REF PendingOps;
--- RansomGuard/RansomGuard/kernelcallbacks.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace kernel_callbacks
-{
-	bool Register();
-	void UnRegister();
-	VOID RansomGuardProcessCallback(HANDLE ParentId, HANDLE ProcessId, BOOLEAN Create);
-
-}
